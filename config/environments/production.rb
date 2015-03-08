3f101598e176f58154cee42629cfde52851e355d
Fntsrlike <fntsrlike@gmail.com>
2015-03-09 04:48:28 +0800

    Change mailler setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'compiler.oolab.fntsr.tw' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.mandrillapp.com",
+      :port => "587",
+      :domain => "mandrillapp.com",
+      :authentication => "plain",
+      :user_name => "example@gmail.com",
+      :password => "123456",
+      :enable_starttls_auto => true
+   }
 end
