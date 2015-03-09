116c84c0a54019edb2ea24581d7a833adfdd7883
Fntsrlike <fntsrlike@gmail.com>
2015-03-09 13:35:12 +0800

    Change status label with color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def puts_answer_label answer
+    if answer.done?
+      status = answer.judgement.result
+      if answer.judgement.pass?
+        label = "green label"
+      elsif answer.judgement.reject?
+        label = "red label"
+      end
+    else
+      status = answer.status
+      if answer.queue?
+        label = "label"
+      elsif answer.judgement?
+        label = "yellow label"
+      end
+    end
+
+    raw "<a class=\"ui #{label}\">#{status}</a>"
+  end
 end
