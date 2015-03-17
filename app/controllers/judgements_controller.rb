f1a8d329ab6eeb508df5fac7403e75c85c6f26a6
Fntsrlike <fntsrlike@gmail.com>
2015-03-17 19:11:23 +0800

    Add answer logs on judgement form

--- app/controllers/judgements_controller.rb
+++ app/controllers/judgements_controller.rb
@@ -24,12 +24,14 @@ class JudgementsController < ApplicationController
       @answer.judgement!
     end
     @judgement = Judgement.new
+    @logs = get_answer_logs
   end
 
   # GET /judgements/1/edit
   def edit
     authorize! :update, @judgement
     @answer = @judgement.answer
+    @logs = get_answer_logs
   end
 
   # POST /judgements
@@ -105,5 +107,23 @@ class JudgementsController < ApplicationController
       grid.update_status
 
       return true
+    end
+
+    def get_answer_logs
+      logs = []
+      quiz = @answer.quiz
+      answers = Answer.where(quiz_id: @answer.quiz_id, user_id: @answer.user_id, status: 2)
+                      .where.not(id: @answer.id).order("created_at ASC")
+      answers.each do | answer |
+        log = {
+          id: answer.id,
+          answer: answer.content,
+          judgement: answer.judgement.content,
+          reviewer: answer.judgement.user,
+          result: answer.judgement.result
+        }
+        logs.push log
+      end
+      return logs
     end
 end
