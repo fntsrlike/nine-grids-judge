d19a7a9d7eebad030fd587e511b1ab3dd62a56a2
Ruoshi Lin <fntsrlike@gmail.com>
2016-02-19 07:32:57 +0800

    Fix Markdown render

--- config/initializers/article.rb
+++ config/initializers/article.rb
@@ -1,48 +1,47 @@
 Dir["#{Rails.root}/lib/redcarpet/*.rb"].each { |file| require file }
 
 module Article
-  MarkdownPipeline = ::HTML::Pipeline.new [
+  MarkdownPipeline = HTML::Pipeline.new [
     # replace @user mentions with links
-    # ::HTML::Pipeline::MentionFilter
+    # HTML::Pipeline::MentionFilter
 
     # replace relative image urls with fully qualified versions
-    # ::HTML::Pipeline::AbsoluteSourceFilter
+    # HTML::Pipeline::AbsoluteSourceFilter
 
     # auto_linking urls in HTML
-    # ::HTML::Pipeline::AutolinkFilter,
+    # HTML::Pipeline::AutolinkFilter,
 
     # replace http image urls with camo-fied https versions
-    # ::HTML::Pipeline::CamoFilter,
+    # HTML::Pipeline::CamoFilter,
 
     # util filter for working with emails
-    # ::HTML::Pipeline::EmailReplyFilter,
+    # HTML::Pipeline::EmailReplyFilter,
 
     # everyone loves emoji!
-    # ::HTML::Pipeline::EmojiFilter,
+    # HTML::Pipeline::EmojiFilter,
 
     # HTML Filter for replacing http github urls with https versions.
-    # ::HTML::Pipeline::HttpsFilter,
+    # HTML::Pipeline::HttpsFilter,
 
-    # link to full size image for large images
-    ::HTML::Pipeline::ImageMaxWidthFilter,
+    # link to full size image for large images (Error)
+    # HTML::Pipeline::ImageMaxWidthFilter,
 
     # convert markdown to html
-    ::HTML::Pipeline::MarkdownFilter,
+    HTML::Pipeline::MarkdownFilter,
 
     # html escape text and wrap the result in a div
-    # ::HTML::Pipeline::PlainTextInputFilter,
+    # HTML::Pipeline::PlainTextInputFilter,
 
     # whitelist sanitize user markup
-    # ::HTML::Pipeline::SanitizationFilter,
+    # HTML::Pipeline::SanitizationFilter,
 
-    # code syntax highlighter
-    ::HTML::Pipeline::SyntaxHighlightFilter,
+    # code syntax highlighter (Error)
+    # HTML::Pipeline::SyntaxHighlightFilter,
 
     # convert textile to html
-    # ::HTML::Pipeline::TextileFilter,
+    # HTML::Pipeline::TextileFilter,
 
     # anchor headings with name attributes and generate Table of Contents html unordered list linking headings
-    # ::HTML::Pipeline::TableOfContentsFilter,
-
-  ], asset_root: ActionController::Base.relative_url_root
+    # HTML::Pipeline::TableOfContentsFilter,
+  ]
 end
